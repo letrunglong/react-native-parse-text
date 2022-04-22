b366409e2c70ad8e0fde2def45e337ffddf1e94d
Long <letrunglong07@gmail.com>
2022-04-22 14:25:29 +0700

    custom props type render text pattern

--- src/ParsedText.d.ts
+++ src/ParsedText.d.ts
@@ -8,7 +8,7 @@ declare module 'react-native-parsed-text' {
       Exclude<keyof TextProps, 'onPress' | 'onLongPress'>
     > {
     /** arbitrary function to rewrite the matched string into something else */
-    renderText?: (matchingString: string, matches: string[]) => string;
+    renderText?: (matchingString: string, matches: string[]) => string| React.ReactNode;
     onPress?: (text: string, index: number) => void;
     onLongPress?: (text: string, index: number) => void;
   }
